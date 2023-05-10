b1478684182a51552f3c9d748de8a3795a7d77b0
Alexander <aleksandarfaraj@users.noreply.github.com>
2023-05-10 11:55:55 +0000

    feat(pkg/tzapconnector): add localdbconnector to partial composite
    This commit adds localdbconnector to the PartialComposite struct in tzapconnector's connector.go file. The purpose is to enable the use of the EmbeddingGenerator methods provided by this connector. The AddEmbeddingDocument, GetEmbeddingDocument, DeleteEmbeddingDocument, SearchWithEmbedding, and ListAllEmbeddingIds methods are included in PartialComposite.
    
    BREAKING CHANGE: This commit does not include any breaking change.
    
    # Explanation
    The PartialComposite struct is used to create a partial implementation of the TGenerator interface. The addition of localdbconnector to the struct enables the use of the EmbeddingGenerator methods provided by this connector. These methods will be used to add, retrieve, and delete embedding documents, as well as to search for embeddings, and list them all.
    
    This change does not introduce any breaking change to the codebase.

--- pkg/tzapconnect/connector.go
+++ pkg/tzapconnect/connector.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/tzapio/tzap/pkg/config"
+	"github.com/tzapio/tzap/pkg/connectors/localdbconnector"
 	"github.com/tzapio/tzap/pkg/connectors/openaiconnector"
 	"github.com/tzapio/tzap/pkg/types"
 )
@@ -27,16 +28,20 @@ func newBaseconnector() (types.TGenerator, error) {
 		return nil, err
 	}
 	openaiC := openaiconnector.InitiateOpenaiClient(apiKey)
-
-	partialComposite := PartialComposite{OpenaiTgenerator: openaiC}
+	embeddingC, err := localdbconnector.InitiateLocalDB("./.tzap-data/fileembeddings.db")
+	if err != nil {
+		return nil, err
+	}
+	partialComposite := PartialComposite{OpenaiTgenerator: openaiC, EmbeddingGenerator: embeddingC}
 	var myInterface types.TGenerator = partialComposite
 	return myInterface, nil
 }
 
 type PartialComposite struct {
-	types.UnimplementedTGenerator
-	OpenaiTgenerator *openaiconnector.OpenaiTgenerator
-	VoiceGenerator   types.TGenerator
+	*types.UnimplementedTGenerator
+	OpenaiTgenerator   *openaiconnector.OpenaiTgenerator
+	VoiceGenerator     types.TGenerator
+	EmbeddingGenerator types.TGenerator
 }
 
 func (pc PartialComposite) TextToSpeech(ctx context.Context, content, language, voice string) (*[]byte, error) {
@@ -45,11 +50,29 @@ func (pc PartialComposite) TextToSpeech(ctx context.Context, content, language, 
 func (pc PartialComposite) GenerateChat(ctx context.Context, messages []types.Message, stream bool) (string, error) {
 	return pc.OpenaiTgenerator.GenerateChat(ctx, messages, stream)
 }
+func (pc PartialComposite) FetchEmbedding(ctx context.Context, content ...string) ([][]float32, error) {
+	return pc.OpenaiTgenerator.FetchEmbedding(ctx, content...)
+}
 func (pc PartialComposite) CountTokens(ctx context.Context, content string) (int, error) {
 	return pc.OpenaiTgenerator.CountTokens(content)
 }
 func (pc PartialComposite) OffsetTokens(ctx context.Context, content string, from int, to int) (string, error) {
 	return pc.OpenaiTgenerator.OffsetTokens(content, from, to)
+}
+func (pc PartialComposite) SearchWithEmbedding(ctx context.Context, embedding types.QueryFilter, k int) (types.SearchResults, error) {
+	return pc.EmbeddingGenerator.SearchWithEmbedding(ctx, embedding, k)
+}
+func (pc PartialComposite) AddEmbeddingDocument(ctx context.Context, docID string, embedding []float32, metadata map[string]string) error {
+	return pc.EmbeddingGenerator.AddEmbeddingDocument(ctx, docID, embedding, metadata)
+}
+func (pc PartialComposite) GetEmbeddingDocument(ctx context.Context, docID string) (types.Vector, bool, error) {
+	return pc.EmbeddingGenerator.GetEmbeddingDocument(ctx, docID)
+}
+func (pc PartialComposite) DeleteEmbeddingDocument(ctx context.Context, docID string) error {
+	return pc.EmbeddingGenerator.DeleteEmbeddingDocument(ctx, docID)
+}
+func (pc PartialComposite) ListAllEmbeddingsIds(ctx context.Context) (types.SearchResults, error) {
+	return pc.EmbeddingGenerator.ListAllEmbeddingsIds(ctx)
 }
 func getOpenAIAPIKeyFromEnv() (string, error) {
 	apiKey := os.Getenv("OPENAI_APIKEY")
