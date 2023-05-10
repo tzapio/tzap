b661d9fc91f167b1ab7efde21f82271d2887ce89
Alexander <aleksandarfaraj@users.noreply.github.com>
2023-05-10 11:09:56 +0000

    feat(pkg/tzapconnect): add stubconnector package
    The stubconnector package is added to provide a connector implementation for testing purposes. It includes support for text to speech, speech to text, generating chat, token counting, and offsetting. Additionally, support for fetching embeddings, searching with embedding, adding embedding documents, getting embedding documents, deleting embedding documents, and listing all embedding ids are provided.

---
+++ pkg/tzapconnect/stubconnector/stubconnector.go
@@ -0,0 +1,52 @@
+package stubconnector
+
+import (
+	"context"
+
+	"github.com/tzapio/tzap/pkg/config"
+	"github.com/tzapio/tzap/pkg/types"
+)
+
+type StubConnector struct {
+}
+
+func StubWithConfig(conf config.Configuration) types.TzapConnector {
+	tg := StubConnector{}
+
+	return func() (types.TGenerator, config.Configuration) {
+		return tg, conf
+	}
+}
+func (StubConnector) TextToSpeech(ctx context.Context, content, language, voice string) (*[]byte, error) {
+	return &[]byte{}, nil
+}
+func (StubConnector) SpeechToText(ctx context.Context, audioContent *[]byte, language string) (string, error) {
+	return "nil", nil
+}
+func (StubConnector) GenerateChat(ctx context.Context, messages []types.Message, stream bool) (string, error) {
+	return "Hello world", nil
+}
+func (StubConnector) CountTokens(ctx context.Context, content string) (int, error) {
+	return len("Hello world"), nil
+}
+func (StubConnector) OffsetTokens(ctx context.Context, content string, from int, to int) (string, error) {
+	return "Hell", nil
+}
+func (StubConnector) FetchEmbedding(ctx context.Context, content ...string) ([][]float32, error) {
+	return [][]float32{{0, 1, 2, 3, 4, 5}}, nil
+}
+func (StubConnector) SearchWithEmbedding(ctx context.Context, embedding types.QueryFilter, k int) (types.SearchResults, error) {
+	return types.SearchResults{}, nil
+}
+func (StubConnector) AddEmbeddingDocument(ctx context.Context, docID string, embedding []float32, metadata map[string]string) error {
+	return nil
+}
+func (StubConnector) GetEmbeddingDocument(ctx context.Context, docID string) (types.Vector, bool, error) {
+	return types.Vector{}, false, nil
+}
+func (StubConnector) DeleteEmbeddingDocument(ctx context.Context, docID string) error {
+	return nil
+}
+func (StubConnector) ListAllEmbeddingsIds(ctx context.Context) (types.SearchResults, error) {
+	return types.SearchResults{}, nil
+}
