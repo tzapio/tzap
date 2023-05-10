79155bd917aa93922b947b6561fcca8306e0d2a5
Alexander <aleksandarfaraj@users.noreply.github.com>
2023-05-10 12:08:42 +0000

    feat: add generatorembedding example
    This commit adds a new example to the repository, "generatorembedding". The example demonstrates how to use the `tzap` package to generate embedding inspirations, utilizing the `tzapconnect` and `files` packages. The new example includes a `generate.go` file, which can be run from the command line with a filename and content as arguments to generate the embedding inspiration, and uses `openai.GPT4` as the model.

---
+++ examples/generatorembedding/generate.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/tzapio/tzap/pkg/config"
+	"github.com/tzapio/tzap/pkg/types/openai"
+	"github.com/tzapio/tzap/pkg/tzap"
+	"github.com/tzapio/tzap/pkg/tzapconnect"
+	"github.com/tzapio/tzap/templates/code/files"
+)
+
+func main() {
+	filename := os.Args[1]
+	println(filename)
+	content := strings.Join(os.Args[2:], " ")
+	tzap.
+		NewWithConnector(
+			tzapconnect.WithConfig(
+				config.Configuration{
+					MD5Rewrites: true,
+					OpenAIModel: openai.GPT4,
+					EnableLogs:  true,
+				})).
+		ApplyTemplate(files.EmbeddingInspirationTemplate(
+			content,
+			[]string{
+				"./pkg/types/interfaces.go",
+				"./pkg/tzap/tzap.go",
+				//	"./templates/code/files/embeddingInspirationTemplate.go",
+			},
+		)).
+		AddUserMessage(content).
+		LoadTaskOrRequestNewTaskMD5(filename)
+}
