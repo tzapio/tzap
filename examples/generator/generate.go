db18cc670d7b2269846854136df90ee85eabdcbc
Alexander <aleksandarfaraj@users.noreply.github.com>
2023-05-10 12:09:43 +0000

    feat: add code generation and testing example
    This commit adds a code generation and testing example to the `examples` directory.
    
    The code generation example uses the `tzap` package to apply the `InspirationTemplate` to a list of files and adds a custom user message. The example also includes an optional command line argument to read from a file.
    
    The testing example uses the `tzap` package to load files from a directory using the `LoadFileDir` method. The example also includes a prompt message for creating unit tests and adds a new template from an external package.
    
    BREAKING CHANGE: The `MD5Rewrites` configuration option in the `TestingTask` configuration has been changed to `false`.

---
+++ examples/generator/generate.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/tzapio/tzap/pkg/config"
+	"github.com/tzapio/tzap/pkg/types/openai"
+	"github.com/tzapio/tzap/pkg/tzap"
+	"github.com/tzapio/tzap/pkg/tzapconnect"
+	"github.com/tzapio/tzap/templates/code/files"
+)
+
+func main() {
+	filename := os.Args[1]
+	content := strings.Join(os.Args[2:], " ")
+	tzap.
+		NewWithConnector(
+			tzapconnect.WithConfig(
+				config.Configuration{
+					MD5Rewrites: true,
+					OpenAIModel: openai.GPT4,
+				})).
+		ApplyTemplate(files.InspirationTemplate(
+			[]string{
+				"README.md",
+				"cli/cmd/semanticgitcommit.go",
+				"pkg/types/structs.go",
+				"pkg/tzap/templates.go",
+				"pkg/tzap/tzap.go",
+				"templates/code/gocode/arguments.go",
+			},
+		)).
+		AddUserMessage(content).
+		LoadTaskOrRequestNewTask(filename)
+}
--- examples/testing/main.go
+++ examples/testing/main.go
@@ -14,23 +14,23 @@ func main() {
 		config.Configuration{
 			AutoMode:    true,
 			OpenAIModel: openai.GPT4,
-			MD5Rewrites: true,
+			MD5Rewrites: false,
 		})).
-		LoadFileDir("/workspaces/goman/tzaps", "*.go").
+		LoadFileDir("./pkg/embed", "*.go").
 		Map(func(t *tzap.Tzap) *tzap.Tzap {
 			return t.
 				ApplyTemplateFN(
 					codegeneration.MakeCodeGO(`
 You are helping the user writing a library for chatgpt prompting. You primarely write Golang. Most files already exists. Do not create new data structures.
-### Current interface: 
-//file: /workspaces/goman/tzaps/interfaces.go
-`+util.ReadFileP("/workspaces/goman/tzaps/interfaces.go")+`
-### General Tzap logic
-//file: /workspaces/goman/tzaps/tzap.go
-`+util.ReadFileP("/workspaces/goman/tzaps/tzap.go")+`
-### Additional types
-//file: /workspaces/goman/tzaps/msg/message.go
-`+util.ReadFileP("/workspaces/goman/tzaps/msg/message.go"),
+
+###file: ./pkg/types/interfaces.go
+`+util.ReadFileP("./pkg/types/interfaces.go")+`
+
+###file: ./pkg/tzap/tzap.go
+`+util.ReadFileP("./pkg/tzap/tzap.go")+`
+
+###file: ./pkg/types/embedding.go
+`+util.ReadFileP("./pkg/types/embedding.go"),
 						"Make unit tests. Use testify go. If needed create tmp files. Use package tzap_test. Use testnames Test_{function}_{givenCamelCase}_{expectCamelCase}."),
 				)
 		})
