a6aadeeba5a4ff6c919f5bae745000b4b9a3e294
Alexander <aleksandarfaraj@users.noreply.github.com>
2023-05-15 09:23:51 +0000

    refactor: simplify DisplayAndConfirm function in stdin.go
    The DisplayAndConfirm function in the stdin.go file was simplified by removing the unnecessary `content` variable assignment and formatting the prompt with a new line character. This makes the code cleaner and easier to read.

--- workflows/code/gocode/stdin.go
+++ workflows/code/gocode/stdin.go
@@ -12,9 +12,8 @@ func DisplayAndConfirm() types.NamedWorkflow[*tzap.Tzap, *tzap.ErrorTzap] {
 	return types.NamedWorkflow[*tzap.Tzap, *tzap.ErrorTzap]{
 		Name: "DisplayAndConfirm",
 		Workflow: func(t *tzap.Tzap) *tzap.ErrorTzap {
-			content := t.Data["content"].(string)
-			fmt.Println("\n", content)
-			ok := stdin.ConfirmPrompt("Continue with this commit?")
+
+			ok := stdin.ConfirmPrompt("\nContinue with this commit?")
 
 			if !ok {
 				return t.ErrorTzap(fmt.Errorf("commit aborted by user"))
