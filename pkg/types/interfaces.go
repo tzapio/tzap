ee3d858cf187f2225a4227394180c76dd8091303
Alexander <aleksandarfaraj@users.noreply.github.com>
2023-05-15 08:41:47 +0000

    feat: update ITzap interface in pkg/types/interfaces.go
    - Renamed SetHeader method to SetInitialSystemContent.
    - Renamed LoadTask method to LoadCompletion.
    - Renamed LoadTaskOrRequestNewTask method to LoadCompletionOrRequestCompletion.
    - Renamed RequestChat method to RequestChatCompletion.
    - Added LoadFiles method that loads multiple files.
    - Added StoreCompletion method that persists completion to a file.
    - Added CloneTzap method that returns a copy of the Tzap object.
    - Renamed ApplyTemplate method to ApplyWorkflow.
    - Renamed NamedTemplate struct to NamedWorkflow.
    - Added ApplyErrorWorkflow method that takes a function returning an error to handle errors.
    
    Also added CountTokens and OffsetTokens methods to the interface to support tokenization functionality.

--- pkg/types/interfaces.go
+++ pkg/types/interfaces.go
@@ -4,42 +4,48 @@ type ITzap[T any, Z any] interface {
 	AddUserMessage(contents ...string) T
 	AddAssistantMessage(contents ...string) T
 	AddSystemMessage(contents ...string) T
-	SetHeader(header string) T
+	SetInitialSystemContent(content string) T
 
-	LoadFileDir(dir string, ext string) T
+	LoadFileDir(dir string) T
+	LoadFiles(filepaths []string) T
 	ChangeFilepath(filepath string) T
-	FetchTask() T
 
-	LoadTask(filepath string) T
-	LoadTaskOrRequestNewTask(filepath string) T
-	LoadTaskOrRequestNewTaskMD5(filepath string) T
+	LoadCompletion(filepath string) T
+	LoadCompletionOrRequestCompletionMD5(filePath string) T
+	LoadCompletionOrRequestCompletion(filepath string) T
 
-	RequestChat() T
+	RequestChatCompletion() T
+	StoreCompletion(filePath string) T
 	RequestTextToSpeech(language, voice string) T
 
 	//Tzap Primitives
 	New() T
 	AddTzap(tc T) T
 	HijackTzap(tc T) T
+	CloneTzap(tc T) T
 
 	//ControlFlow
 	WorkTzap(fn func(t T)) T
 	IsolatedTzap(fn func(t T)) T
 	MutationTzap(fn func(t T) T) T
-	Map(func(T) T) T
-	Accumulate(func(T) T) T
+	Map(func(t T) T) T
+	Accumulate(func(t T) T) T
 	Exit() T
 
-	ApplyTemplate(nt NamedTemplate[T, Z]) T
-	ApplyTemplateFN(nt func(T) T) T
-	ApplyTemplateP(T) T
+	ApplyWorkflow(nt NamedWorkflow[T, Z]) T
+	ApplyErrorWorkflow(nt NamedWorkflow[T, Z], fn func(t Z) error) T
+	ApplyWorkflowFN(nt func(t T) T) T
+	ApplyWorkflowP(T) T
 
 	Recursive(func(tzapThatCreatesNewChildren T) T) T
 	CheckAndHandleGlobalOccurrences(references int, filename string, noOccurrence, handleOccurrence func(T) T) T
 	CheckAndHandleRecurrences(references int, filename string, noReccurance, handleRecurrence func(T) T) T
 	FileMustContainHandleGlobalOccurrences(references int, filename string, noOccurrence, handleOccurrence func(T) T) T
+
+	CountTokens(content string) (int, error)
+	OffsetTokens(content string, from int, to int) (string, error)
 }
-type NamedTemplate[T any, Z any] struct {
+type NamedWorkflow[T any, Z any] struct {
 	Name     string
-	Template func(T) Z // Assuming the function takes no arguments and returns no values
+	Workflow func(t T) Z // Assuming the function takes no arguments and returns no values
 }
