87b194a7b051c4676ee143f666c612c1a89456fd
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-23 21:12:15 +0000

    feat(codegen): refactor makecode function and remove redundant go and ts implementations
    This commit refactors the `MakeCode` function in the `codegeneration` package to be more generic and allows it to generate code in any language. The function now takes the input and output file extensions as arguments and generates the output file by replacing the input file extension with the output file extension. The commit also removes the redundant `MakeCodeGO` and `MakeCodeTS` functions and their implementations.
    
    BREAKING CHANGE: The `MakeCode` function signature has changed and now requires the language, input and output file extensions, mission and task arguments to be explicitly passed in. The removed functions `MakeCodeGO` and `MakeCodeTS` are no longer available. Code generators invoking these functions need to be updated to use the new `MakeCode` signature.

--- workflows/code/codegeneration/makecode.go
+++ workflows/code/codegeneration/makecode.go
@@ -1,20 +1,21 @@
 package codegeneration
 
 import (
+	"path"
 	"strings"
 
 	"github.com/tzapio/tzap/pkg/tzap"
 )
 
-func MakeCodeTS(mission, task string) func(t *tzap.Tzap) *tzap.Tzap {
+func MakeCode(language, extensionIn, extensionOut, mission, task string) func(t *tzap.Tzap) *tzap.Tzap {
 	return func(t *tzap.Tzap) *tzap.Tzap {
 		filein := t.Data["filepath"].(string)
-		fileout := strings.TrimSuffix(t.Data["filepath"].(string), ".ts") + ".go"
-		if strings.HasSuffix(filein, "_test.go") || strings.Contains(filein, "file2") {
+		if path.Ext(filein) != extensionIn {
 			return t
 		}
+		fileout := strings.TrimSuffix(t.Data["filepath"].(string), extensionIn) + extensionOut
 		return t.
-			HijackTzap(&tzap.Tzap{Name: "MakeCodeTS"}).
+			HijackTzap(&tzap.Tzap{Name: "MakeCodeGO"}).
 			AddSystemMessage("### The overall mission: \n"+mission).
 			AddUserMessage(
 				"###",
@@ -22,16 +23,20 @@ func MakeCodeTS(mission, task string) func(t *tzap.Tzap) *tzap.Tzap {
 				"PLAN: Do not write any text because this file will be saved directly to "+fileout,
 				"TASKFILE: "+filein,
 				"OUTFILE: "+fileout,
-				"OUTPUT: golang",
+				"OUTPUT: "+language,
 				"### EXAMPLE:",
 				"EXAMPLE:",
-				"{golang code}").
+				"{"+language+" code}").
 			AddSystemMessage(
 				"###",
-				"//file: "+filein+"\n",
+				"###file: "+filein+"\n",
 				t.Data["content"].(string),
 			).
 			LoadCompletionOrRequestCompletionMD5(fileout)
 
+		/*"These are the current existing files:\n//existing file (content redacted)",
+		strings.Join(files, "\n//existing file (content redacted)"),*/
+
 	}
+
 }
