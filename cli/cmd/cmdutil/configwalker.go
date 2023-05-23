e592e3e0c35d6da4fcc0bfabb28164ecec8789a9
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-23 16:15:16 +0000

    feat(cli): add SearchForTzapincludeAndChangeDir function
    This commit adds a new function, SearchForTzapincludeAndChangeDir, to the cli/cmd/cmdutil package. The function is used to search for a ".tzapinclude" file in the current directory and its ancestors. If the file is found, the function changes the working directory to the directory containing the file. This is useful for a command line tool where configuration is done via a file located near the root of the project.

---
+++ cli/cmd/cmdutil/configwalker.go
@@ -0,0 +1,41 @@
+package cmdutil
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func SearchForTzapincludeAndChangeDir() error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get the current directory: %v", err)
+	}
+	cur := cwd
+	for {
+
+		includePath := filepath.Join(cur, ".tzapinclude")
+		if _, err := os.Stat(includePath); err == nil {
+			break
+		} else if !os.IsNotExist(err) {
+			return fmt.Errorf("unexpected error when checking for .tzapinclude in %s: %v", cur, err)
+		}
+
+		if cur == filepath.Dir(cur) {
+			fmt.Println("Reached the top of the directory tree.")
+			break
+		}
+		fmt.Printf("Did not find .tzapinclude in: %s\n", cur)
+		cur = filepath.Dir(cur)
+	}
+
+	if cur != cwd {
+		fmt.Printf("Changing working directory to: %s\n", cur)
+
+		if err := os.Chdir(cur); err != nil {
+			return fmt.Errorf("failed to change the directory to %s: %v", cur, err)
+		}
+	}
+
+	return nil
+}
