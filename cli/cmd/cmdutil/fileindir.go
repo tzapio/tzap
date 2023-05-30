73d9bf8aba9ea2035b020726b8f769ce431ad021
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-30 11:07:43 +0000

    feat(cmdutil): Add FileInDirEvaluator to evaluate file inclusion/exclusion rules
    This commit adds a new type `FileInDirEvaluator` which is responsible for evaluating which files and directories should be included in a given file tree, based on `.gitignore`, `.tzapignore`, and `.tzapinclude` files. It contains methods to traverse a directory using rules defined in an exclude list, and also get all the files in a tree while applying inclusion and exclusion rules. It allows users to provide custom inclusion/exclusion rules by passing arguments.
    
    This is a new feature that will not affect backward compatibility, hence the `feat` type has been used.
    
    BREAKING CHANGE: N/A.

--- cli/cmd/cmdutil/fileindir.go
+++ cli/cmd/cmdutil/fileindir.go
@@ -1,29 +1,90 @@
 package cmdutil
 
-import "os"
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 
-func GetNonExcludedFiles(files []string) []string {
-	baseIgnore := []string{".git/", ".DS_Store", "desktop.ini"}
+	ignore "github.com/sabhiram/go-gitignore"
+	"github.com/tzapio/tzap/internal/logging/tl"
+)
+
+type FileInDirEvaluator struct {
+	includeMatcher *ignore.GitIgnore
+	excludeMatcher *ignore.GitIgnore
+}
+
+func NewFileInDirEvaluator() (*FileInDirEvaluator, error) {
+	excludePatterns := []string{".git", ".DS_Store", "desktop.ini"}
 	ignoreFiles := []string{".tzapignore"}
-	if _, err := os.Stat(".gitignore"); os.IsNotExist(err) {
+	if _, err := os.Stat(".gitignore"); err == nil {
 		ignoreFiles = append(ignoreFiles, ".gitignore")
 	}
-	excludePatterns, err := ReadFilterPatternFiles(ignoreFiles...)
+	patterns, err := ReadFilterPatternFiles(ignoreFiles...)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-
-	excludePatterns = append(excludePatterns, baseIgnore...)
-	files = FilterWithExcludePattern(files, excludePatterns)
+	excludePatterns = append(excludePatterns, patterns...)
 
 	includePatterns, err := ReadFilterPatternFiles(".tzapinclude")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	files = FilterWithIncludePattern(files, includePatterns)
 
-	var kilobyte = 1024
-	files = FilterFileSize(files, 10000*kilobyte)
-
-	return files
+	return NewFileInDirEvaluatorWithPatterns(excludePatterns, includePatterns), nil
+}
+func NewFileInDirEvaluatorWithPatterns(excludePatterns []string, includePatterns []string) *FileInDirEvaluator {
+	excludeMatcher := ignore.CompileIgnoreLines(excludePatterns...)
+	includeMatcher := ignore.CompileIgnoreLines(includePatterns...)
+	return &FileInDirEvaluator{excludeMatcher: excludeMatcher, includeMatcher: includeMatcher}
+}
+func (e *FileInDirEvaluator) ShouldKeepPath(path string) bool {
+	exclude := e.excludeMatcher.MatchesPath(path)
+	include := e.includeMatcher.MatchesPath(path)
+	return include && !exclude
+}
+func (e *FileInDirEvaluator) ShouldTraverseDir(path string) bool {
+	return !e.excludeMatcher.MatchesPath(path)
+}
+func (e *FileInDirEvaluator) WalkDir(dir string) ([]string, error) {
+	var list []string
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		tl.Logger.Println("WALKDIR", dir, path, d.Name())
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			if e.ShouldTraverseDir(path) || d.Name() == "." {
+				tl.Logger.Println("KEEPDIR", path)
+				return nil
+			} else {
+				tl.Logger.Println("SKIPDIR", path)
+				return filepath.SkipDir
+			}
+		} else if e.ShouldKeepPath(path) {
+			tl.Logger.Println("KEEPFILE", path)
+			absPath, err := filepath.Abs(path)
+			if err != nil {
+				return err
+			}
+			relPath, err := filepath.Rel(cwd, absPath)
+			if err != nil {
+				return err
+			}
+			relPath = strings.TrimPrefix(relPath, "./")
+			list = append(list, filepath.ToSlash(relPath))
+			return nil
+		}
+		tl.Logger.Println("SKIPFILE", path)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
 }
