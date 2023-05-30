c0d65893467eae067957e5608d805c4781e81940
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-30 11:10:31 +0000

    feat(util): add MD5HashByte method for byte array hashing
    This commit adds a new method to the utils package, MD5HashByte, that takes a byte array as input and returns the corresponding MD5 hash as a hex-encoded string. This method can be used for hashing binary data, in contrast to the existing MD5Hash method which takes a string as input.

--- pkg/util/hash.go
+++ pkg/util/hash.go
@@ -9,3 +9,7 @@ func MD5Hash(text string) string {
 	hash := md5.Sum([]byte(text))
 	return hex.EncodeToString(hash[:])
 }
+func MD5HashByte(text []byte) string {
+	hash := md5.Sum(text)
+	return hex.EncodeToString(hash[:])
+}
