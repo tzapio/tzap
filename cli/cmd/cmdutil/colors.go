cb0820501fc17ef1e7cf6640afe981b755498c3c
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-30 10:50:43 +0000

    feat(cli): update color functions in CLI
    BREAKING CHANGE: The `cyan`, `bold`, and `yellow` color functions in the `cli/cmd` package have been replaced with `Cyan`, `Bold`, and `Yellow` color functions in the `cli/cmd/cmdutil` package.
    
    This commit removes the `cli/cmd/colors.go` file and replaces it with a new `cli/cmd/cmdutil/colors.go` file that contains the updated color functions. These new functions have been renamed to use uppercase first letters to conform to standard naming conventions, and they use the `SprintFunc` method from the `github.com/fatih/color` package to format and colorize text.
    
    This is a breaking change because any code that depends on the old `cli/cmd` color functions will need to be updated to use the new `cli/cmd/cmdutil` color functions.

---
+++ cli/cmd/cmdutil/colors.go
@@ -0,0 +1,9 @@
+package cmdutil
+
+import "github.com/fatih/color"
+
+type colorFunction func(a ...interface{}) string
+
+var Cyan colorFunction = color.New(color.FgCyan).SprintFunc()
+var Bold colorFunction = color.New(color.Bold).SprintFunc()
+var Yellow colorFunction = color.New(color.FgYellow).SprintFunc()
--- cli/cmd/colors.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-import "github.com/fatih/color"
-
-type colorFunction func(a ...interface{}) string
-
-var cyan colorFunction = color.New(color.FgCyan).SprintFunc()
-var bold colorFunction = color.New(color.Bold).SprintFunc()
-var yellow colorFunction = color.New(color.FgYellow).SprintFunc()
