294779a940e6fafa8889997b75f89fdeec535ace
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-30 11:06:45 +0000

    feat(cmdutil): rename SearchForTzapincludeAndChangeDir to SearchForTzapincludeAndGetRootDir
    This commit renames the function SearchForTzapincludeAndChangeDir in configwalker.go to SearchForTzapincludeAndGetRootDir in order to better reflect the function's purpose of searching for a .tzapinclude file and returning the root directory of the project. Additionally, the function now returns the root directory instead of returning an error or changing the working directory.
    
    BREAKING CHANGE: The return value of SearchForTzapincludeAndChangeDir has been changed from an error to a string, which now represents the root directory of the project. This may affect code that relies on the previous behavior of the function.

--- cli/cmd/cmdutil/configwalker.go
+++ cli/cmd/cmdutil/configwalker.go
@@ -6,10 +6,10 @@ import (
 	"path/filepath"
 )
 
-func SearchForTzapincludeAndChangeDir() error {
+func SearchForTzapincludeAndGetRootDir() (string, error) {
 	cwd, err := os.Getwd()
 	if err != nil {
-		return fmt.Errorf("failed to get the current directory: %v", err)
+		return "", fmt.Errorf("failed to get the current directory: %v", err)
 	}
 	cur := cwd
 	for {
@@ -18,23 +18,20 @@ func SearchForTzapincludeAndChangeDir() error {
 		if _, err := os.Stat(includePath); err == nil {
 			break
 		} else if !os.IsNotExist(err) {
-			return fmt.Errorf("unexpected error when checking for .tzapinclude in %s: %v", cur, err)
+			return "", fmt.Errorf("unexpected error when checking for .tzapinclude in %s: %v", cur, err)
 		}
 
 		if cur == filepath.Dir(cur) {
 			panic("Reached the top of the directory tree. Did not find .tzapinclude in any parent directory. Did you run 'tzap init'?")
 		}
-		fmt.Printf("Did not find .tzapinclude in: %s\n", cur)
+		println("Did not find .tzapinclude in: " + cur)
 		cur = filepath.Dir(cur)
 	}
 
 	if cur != cwd {
-		fmt.Printf("Changing working directory to: %s\n", cur)
-
-		if err := os.Chdir(cur); err != nil {
-			return fmt.Errorf("failed to change the directory to %s: %v", cur, err)
-		}
+		println("Changing working directory to: " + cur)
+		return cur, nil
 	}
 
-	return nil
+	return cwd, nil
 }
