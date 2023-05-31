d99a29f9424e904f63ee1bc78c729a7fb659e31c
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-05-31 22:12:32 +0000

    feat(tokenizer): add asynchronous initialization and encapsulation
    This commit adds asynchronous initialization using singlewait package, to improve performance and encapsulate the tokenizer functionality within the Tokenizer struct. The tokenizer instance is now a member of the Tokenizer struct, which is instantiated using the NewTokenizer function. The CountTokens and OffsetTokens methods are now methods of the Tokenizer struct, while the RawTokens function remains a standalone function.

--- pkg/connectors/openaiconnector/tokenizer/tokenizer.go
+++ pkg/connectors/openaiconnector/tokenizer/tokenizer.go
@@ -7,28 +7,36 @@ import (
 	"github.com/tzapio/tokenizer/codec"
 	enc "github.com/tzapio/tokenizer/codec/cl100k_base"
 	"github.com/tzapio/tzap/internal/logging/tl"
+	"github.com/tzapio/tzap/pkg/embed/localdb/singlewait"
 )
 
-var _tokenizer codec.Codec
-
-func init() {
-	/*enc, err := tiktokenizer.Get(tiktokenizer.Cl100kBase)
-	if err != nil {
-		panic("error getting tokenizer")
-	}*/
-	_tokenizer = *enc.NewCl100kBase()
+type Tokenizer struct {
+	tokenizer *singlewait.SingleWait[*codec.Codec]
 }
 
-func CountTokens(content string) (int, error) {
-	ids, _, err := _tokenizer.Encode(content)
+func NewTokenizer() *Tokenizer {
+	waitTokenizer := singlewait.New(func() *codec.Codec {
+		tl.Logger.Println("Initiating Tokenizer Client")
+		tokenizer := enc.NewCl100kBase()
+		tl.Logger.Println("Done - Initializing Tokenizer Client")
+		return tokenizer
+	})
+	t := &Tokenizer{
+		tokenizer: waitTokenizer,
+	}
+
+	return t
+}
+func (t *Tokenizer) CountTokens(content string) (int, error) {
+	ids, _, err := t.tokenizer.GetData().Encode(content)
 	if err != nil {
 		return 0, errors.New("error couting tokens while encoding")
 	}
 	return len(ids), err
 }
 
-func OffsetTokens(content string, from int, to int) (string, int, error) {
-	ids, strs, err := _tokenizer.Encode(content)
+func (t *Tokenizer) OffsetTokens(content string, from int, to int) (string, int, error) {
+	ids, strs, err := t.tokenizer.GetData().Encode(content)
 	if err != nil {
 		return "", 0, errors.New("error couting tokens while encoding")
 	}
@@ -44,6 +52,6 @@ func OffsetTokens(content string, from int, to int) (string, int, error) {
 	return s, len(offsetStrs), err
 }
 
-func RawTokens(content string) ([]string, error) {
+func (t *Tokenizer) RawTokens(content string) ([]string, error) {
 	return []string{}, nil
 }
