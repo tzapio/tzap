61ec034a3723f0b9193cd61ea9b246ea86838cf8
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-02 16:26:36 +0000

    feat: refactor indexing and embedding workflows
    Refactor the IndexFilesAndEmbeddings workflow to call a new LoadAndFetchEmbeddings workflow instead of implementing the embedding retrieval logic itself. Remove unused ConfirmEmbeddingSearch workflow and corresponding import.
    
    BREAKING CHANGE: The IndexFilesAndEmbeddings workflow no longer implements the embedding retrieval logic, and a new LoadAndFetchEmbeddings workflow is introduced. Any code that relied on the previous implementation of IndexFilesAndEmbeddings will need to be updated to use the new workflow.

--- cli/cmd/cliworkflows/stdin.go
+++ cli/cmd/cliworkflows/stdin.go
@@ -11,7 +11,6 @@ import (
 	"github.com/tzapio/tzap/pkg/types"
 	"github.com/tzapio/tzap/pkg/tzap"
 	"github.com/tzapio/tzap/pkg/util"
-	"github.com/tzapio/tzap/pkg/util/stdin"
 	"github.com/tzapio/tzap/workflows/code/embedworkflows"
 )
 
@@ -34,50 +33,7 @@ func IndexFilesAndEmbeddings(dir string, disableIndex, yes bool) types.NamedWork
 			}
 			tl.Logger.Println("Finished index files...")
 
-			return t.ApplyWorkflow(LoadAndFetchEmbeddings(files, embedder, disableIndex, yes))
-		},
-	}
-}
-
-func ConfirmEmbeddingSearch(yes bool) types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap] {
-	return types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap]{
-		Name: "confirmEmbeddingSearch",
-		Workflow: func(t *tzap.Tzap) *tzap.Tzap {
-			uncachedEmbeddings, ok := t.Data["uncachedEmbeddings"].(types.Embeddings)
-			if !ok {
-				panic("Loading embeddings went wrong")
-			}
-			if len(uncachedEmbeddings.Vectors) > 19 {
-				price := float64(len(uncachedEmbeddings.Vectors)*400) * 0.0004 / 1000
-				if !yes {
-					ok := stdin.ConfirmPrompt(fmt.Sprintf(
-						"Embeddings - You are about to fetch %d embeddings. Proceed? Estimation tokens: %d. Price is: $0.0004 per 1000 tokens. Estimating %.4f USD",
-						len(uncachedEmbeddings.Vectors), len(uncachedEmbeddings.Vectors)*400, price))
-					if !ok {
-						println("Fetching embeddings aborted by user")
-						os.Exit(0)
-					}
-				}
-			}
-			return t
-		},
-	}
-}
-func LoadAndFetchEmbeddings(files []types.FileReader, embedder *embed.Embedder, disableIndex, yes bool) types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap] {
-	return types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap]{
-		Name: "loadAndFetchEmbeddings",
-		Workflow: func(t *tzap.Tzap) *tzap.Tzap {
-			return t.MutationTzap(func(t *tzap.Tzap) *tzap.Tzap {
-				if !disableIndex {
-					println("Checking for file changes. (use -d to disable this check)...\n")
-					return t.
-						ApplyWorkflow(embedworkflows.PrepareEmbedFilesWorkflow(files, embedder)).
-						ApplyWorkflow(ConfirmEmbeddingSearch(yes)).
-						ApplyWorkflow(embedworkflows.FetchOrCachedEmbeddingForFilesWorkflow()).
-						ApplyWorkflow(embedworkflows.SaveAndLoadEmbeddingsToDB())
-				}
-				return t
-			})
+			return t.ApplyWorkflow(embedworkflows.LoadAndFetchEmbeddings(files, embedder, disableIndex, yes))
 		},
 	}
 }
@@ -87,7 +43,7 @@ func PrintInspirationFiles(inspirationFiles []string) types.NamedWorkflow[*tzap.
 		Workflow: func(t *tzap.Tzap) *tzap.Tzap {
 			return t.WorkTzap(func(t *tzap.Tzap) {
 				if len(inspirationFiles) == 0 {
-					println(cmdutil.Bold("Inspiration files: None (use --inspiration to add more)"))
+					println(cmdutil.Bold("\nInspiration files: None (use --inspiration to add more)"))
 					return
 				}
 				println(cmdutil.Bold("\nInspiration files:"))
