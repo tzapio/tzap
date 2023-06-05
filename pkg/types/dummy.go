d56344c572ca8949384185c446feba8418e40b59
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-05 12:32:27 +0000

    chore(pkg/types): remove unused methods from UnimplementedTGenerator
    This commit removes the `TextToSpeech`, `SpeechToText`, and `GenerateChat` methods from the `UnimplementedTGenerator` type in `pkg/types/dummy.go` as they are not currently used and are likely not implemented yet.

--- pkg/types/dummy.go
+++ pkg/types/dummy.go
@@ -1,17 +1,5 @@
 package types
 
-import "context"
-
 type UnimplementedTGenerator struct {
 	TGenerator
-}
-
-func (UnimplementedTGenerator) TextToSpeech(ctx context.Context, content, language, voice string) (*[]byte, error) {
-	panic("UnimplementedTGenerator - Probably not supported yet.")
-}
-func (UnimplementedTGenerator) SpeechToText(ctx context.Context, audioContent *[]byte, language string) (string, error) {
-	panic("UnimplementedTGenerator - Probably not supported yet.")
-}
-func (UnimplementedTGenerator) GenerateChat(ctx context.Context, messages []Message, stream bool) (string, error) {
-	panic("UnimplementedTGenerator - Probably not supported yet.")
 }
