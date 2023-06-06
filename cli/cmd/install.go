4cfd477ea8d7d08c6cbe5deab4915092c45feb17
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-06 11:05:52 +0000

    feat(cli): add install command
    This commit adds a new `install` command to the CLI. It allows users to install git packages by providing a name and a zip URL. The command initializes `Tzap` with the projectDB and then applies the `IndexZipFilesAndEmbeddings` workflow to embed the packages.
    
    BREAKING CHANGE: This commit does not introduce any breaking changes.

---
+++ cli/cmd/install.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"path"
+
+	"github.com/spf13/cobra"
+	"github.com/tzapio/tzap/cli/cmd/cliworkflows"
+	"github.com/tzapio/tzap/internal/logging/tl"
+	"github.com/tzapio/tzap/pkg/types"
+	"github.com/tzapio/tzap/pkg/tzap"
+)
+
+func init() {
+	RootCmd.AddCommand(installCmd)
+}
+
+var installCmd = &cobra.Command{
+	Aliases: []string{"i"},
+	Use:     "install <name> <zip url>",
+	Short:   "ALPHA: Install git packages",
+	Args:    cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		tl.Logger.Println("Cobra CLI Install start")
+		var name types.ProjectName = types.ProjectName(args[0])
+		var projectDir types.ProjectDir = types.ProjectDir(path.Join("./.tzap-data/", string(name)))
+		var zipURL = args[1]
+
+		err := tzap.HandlePanic(func() {
+			projectDB := types.ProjectDB{}
+			projectDB[name] = projectDir
+			t, err := initializeTzap(projectDB)
+			if err != nil {
+				panic(err)
+			}
+			defer t.HandleShutdown()
+			t.ApplyWorkflow(cliworkflows.IndexZipFilesAndEmbeddings(name, projectDir, zipURL, false, false))
+		})
+
+		if err != nil {
+			panic(err)
+		}
+	},
+}
