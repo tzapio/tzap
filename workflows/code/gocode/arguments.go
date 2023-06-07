d1ef5513416c52d35aaeb4a0406aad4adee04880
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-07 19:32:01 +0000

    feat: add data initialization for deserialized arguments in workflow
    This commit adds a data initialization for deserialized arguments in workflow by making use of a map of interface. This enables the storage of a string value as key in the map.

--- workflows/code/gocode/arguments.go
+++ workflows/code/gocode/arguments.go
@@ -11,6 +11,7 @@ func DeserializedArguments(dataname string, args []string) types.NamedWorkflow[*
 	return types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap]{
 		Name: "deserializedArguments",
 		Workflow: func(t *tzap.Tzap) *tzap.Tzap {
+			t.Data = make(types.MappedInterface)
 			t.Data[dataname] = strings.Join(args, " ")
 			return t
 		}}
