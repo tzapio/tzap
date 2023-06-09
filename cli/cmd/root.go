f7ef91b7da9416f8942654ba7ba5332aca465aaf
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-09 10:56:40 +0000

    fix(cli): update default editor flag value
    The default value for the editor flag has been changed from "stdin" to "vscode". This is to provide a better user experience and avoid confusion when selecting the editor mode. The CHANGELOG has been updated to reflect this change.

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -154,5 +154,5 @@ func init() {
 	RootCmd.PersistentFlags().BoolVarP(&settings.Verbose, "verbose", "v", false, "Enable verbose logging")
 	RootCmd.PersistentFlags().BoolVar(&settings.ApiMode, "api", false, "ALPHA: Enable clean stdout outputs. Also turns off editor mode.")
 	RootCmd.PersistentFlags().BoolVarP(&settings.Yes, "yes", "y", false, "Answer yes on CLI related prompts - cost or similar related questions")
-	RootCmd.PersistentFlags().StringVarP(&settings.Editor, "editor", "e", "stdin", "ALPHA: Select editor mode (stdin, editor, vscode, vim, nano, api).")
+	RootCmd.PersistentFlags().StringVarP(&settings.Editor, "editor", "e", "vscode", "ALPHA: Select editor mode (stdin, editor, vscode, vim, nano).")
 }
