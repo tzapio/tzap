8c83a8bd7cf0812efa31688957edc33eed7e9dba
Alexander Faraj <aleksandarfaraj@users.noreply.github.com>
2023-06-19 15:39:16 +0000

    feat(cli): add SearchWorkflow function to handle search queries

--- cli/action/search.go
+++ cli/action/search.go
@@ -5,10 +5,12 @@ import (
 	"github.com/tzapio/tzap/cli/cmd/cliworkflows"
 	"github.com/tzapio/tzap/internal/logging/tl"
 	"github.com/tzapio/tzap/pkg/embed"
+	"github.com/tzapio/tzap/pkg/embed/embedstore"
 	"github.com/tzapio/tzap/pkg/types"
 	"github.com/tzapio/tzap/pkg/tzap"
 	"github.com/tzapio/tzap/pkg/util/singlewait"
 	"github.com/tzapio/tzap/workflows/code/embedworkflows"
+	"github.com/tzapio/tzap/workflows/code/fileworkflows"
 )
 
 type LoadAndSearchEmbeddingsArgs struct {
@@ -53,6 +55,39 @@ func LoadAndSearchEmbeddingsWorkflow(args *actionpb.SearchArgs) types.NamedWorkf
 			return t.
 				ApplyWorkflow(cliworkflows.IndexFilesAndEmbeddings(args.DisableIndex, args.Yes)).
 				ApplyWorkflow(embedworkflows.SearchFilesWorkflow(queryWait.GetData(), args.ExcludeFiles, int(args.EmbedsCount), int(args.NCount)))
+		},
+	}
+}
+
+// PromptWorkflow defines a workflow for generating code based on code-searching existing files and user input.
+// This workflow creates a chat interface for the user to prompt for their desired output.
+// The inspiration files parameter allows the user to recommend needed inspiration files to enhance GPT's general understanding.
+// The search results are generated using a configurable number of embeddings and ranked by relevance.
+// The user is then prompted to select a single search result. Once selected, the selected search result is used to generate the final desired output. Finally, the workflow is terminated when the completion of the user's message thread is reached.
+func SearchWorkflow(promptWorkflowArgs PromptWorkflowArgs) types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap] {
+	return types.NamedWorkflow[*tzap.Tzap, *tzap.Tzap]{
+		Workflow: func(t *tzap.Tzap) *tzap.Tzap {
+			loadAndSearchEmbeddingsArgs := &actionpb.SearchArgs{
+				ExcludeFiles: promptWorkflowArgs.InspirationFiles,
+				SearchQuery:  promptWorkflowArgs.SearchQuery,
+				EmbedsCount:  promptWorkflowArgs.EmbedsCount,
+				NCount:       promptWorkflowArgs.NCount,
+				DisableIndex: promptWorkflowArgs.DisableIndex,
+				Yes:          promptWorkflowArgs.Yes,
+			}
+
+			return t.
+				// Include all full length inspiration files.
+				ApplyWorkflow(cliworkflows.PrintInspirationFiles(promptWorkflowArgs.InspirationFiles)).
+				ApplyWorkflow(fileworkflows.InspirationWorkflow(promptWorkflowArgs.InspirationFiles)).
+				// Find all embeddings search them, returning the top results.
+				ApplyWorkflow(LoadAndSearchEmbeddingsWorkflow(loadAndSearchEmbeddingsArgs)).
+				MutationTzap(func(t *tzap.Tzap) *tzap.Tzap {
+					searchResult := embedstore.TightenSearchResults(t.Data["searchResults"].(types.SearchResults).Results)
+					return t.
+						ApplyWorkflow(cliworkflows.PrintEmbeddings(searchResult)).
+						ApplyWorkflow(embedworkflows.EmbedWorkflow(searchResult))
+				})
 		},
 	}
 }
